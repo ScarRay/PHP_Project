58e41b9d02874af769ebd69088002b8947add876
ScarRay <paul.cabellan@gmail.com>
2016-08-25 11:05:40 +0200

    kek news

--- view/citizen/citizen.php
+++ view/citizen/citizen.php
@@ -3,7 +3,6 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Mon super site</title>
     <link href="/PHP_Project/css/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <script src="/PHP_Project/js/jquery-1.12.3.js"></script>
     <script src="/PHP_Project/css/bootstrap/js/bootstrap.min.js"></script>
